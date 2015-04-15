f3ac56eccf2a4019740e1ce06721085b540436b0
sgharahi <soheil.gharahi@gmail.com>
2015-04-15 18:00:11 -0500

    2nd iteration

--- distance.pl
+++ distance.pl
@@ -2,10 +2,15 @@
 
 $victim_file = 'mem_trace.out';
 $attacker_file = 'dcache_trace.out';
-open(FILE, $file) or die("Could not open file!");
-while(<FILE>){
+open(V_FILE, $victim_file) or die("Could not open file!");
+open(A_FILE, $attacker_file) or die("Could not open file!");
+
+
+while(<A_FILE>){
 	chomp;
-	my $data = $_;
+	my $a_data = $_;
+	my $v_data = <V_FILE>;
+	
 	my @values = split(';', $data);
 	print "$values[0]\n";	
 
