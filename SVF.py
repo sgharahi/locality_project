c4d22883ce7c0f9c8ee012f78fce172417b05afd
rnprakash <rohithnprakash@gmail.com>
2015-04-18 16:46:06 -0500

    changes

--- SVF.py
+++ SVF.py
@@ -4,16 +4,13 @@ import math
 import numpy as np
 import sys
 
-v_sim = []
-a_sim = []
-
-def gen_smat_v(int_a, int_b, i, j):
+def gen_smat_v(int_a, int_b, i, j, v_sim):
     if(i > j):
         v_sim[i][j] = DTW(int_a, int_b)
     else:
         v_sim[i][j] = 0
 
-def gen_smat_a(int_a, int_b, i, j):
+def gen_smat_a(int_a, int_b, i, j, a_sim):
     if(i > j):
         a_sim[i][j] = abs(int_a - int_b)
     else:
@@ -36,7 +33,7 @@ def DTW(int_a, int_b):
 
     return dtw[len(int_a)][len(int_b)]
 
-def SVF():
+def SVF(v_sim, a_sim):
     v_mean = np.mean(v_sim)
     a_mean = np.mean(a_sim)
     v_std = np.std(v_sim)
@@ -60,23 +57,24 @@ def main():
     with open(v_file) as f:
         for line in f:
             next = line.split(" ")
-            v_trace.append(next)
+            for item in next:
+                v_trace.append(int(item))
     
     with open(a_file) as f:
         for next in f:
-            a_trace.extend(next)
+            a_trace.append(int(next))
 
     v_sim = np.zeros((len(v_trace), len(v_trace)))
     a_sim = np.zeros((len(a_trace), len(a_trace)))
 
     for i in range(0, len(v_trace)):
-        for j in range(0, len(v_trace)):
-            gen_smat_v(v_trace[i], v_trace[j], i, j)
+        for j in range(0, len(v_trace[0])):
+            gen_smat_v(v_trace[i], v_trace[j], i, j, v_sim)
 
     for i in range(0, len(a_trace)):
-        for j in range(0, len(a_trace)):
-            gen_smat_a(a_trace[i], a_trace[j], i, j)
+        for j in range(0, len(a_trace[0])):
+            gen_smat_a(a_trace[i], a_trace[j], i, j, a_sim)
 
-    SVF()
+    print SVF(v_sim, a_sim)
 
 main()
