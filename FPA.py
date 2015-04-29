0f68b9cd35503ed6bc3be7e8cc725dbc719fe550
rnprakash <rohithnprakash@gmail.com>
2015-04-29 18:27:36 -0500

    FPA

---
+++ FPA.py
@@ -0,0 +1,20 @@
+#! /bin/python
+
+# Find out \Delta_2(Q) - L2 sensitivity of Q
+
+def l2_sensitivity(Q):
+
+def LPA(F, _lambda):
+    
+
+def main():
+    epsilon = 0.1
+    _lambda = Math.sqrt(k) * l2_sensitivity(Q) / epsilon
+    
+    #Calculate LPA(F^k, \lambda)
+
+    output 
+    for f in F_k:
+        noise = LPA(F, _lambda)
+        
+    
