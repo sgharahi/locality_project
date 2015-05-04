39e07a8e41a6dd8011cc462d5015292c736f25a6
rnprakash <rohithnprakash@gmail.com>
2015-05-04 14:07:53 -0500

    Lots of changes. FPA + sensitivity testing

--- FPA.py
+++ FPA.py
@@ -1,4 +1,8 @@
-#! /bin/python
+#! /usr/bin/python
+
+import sys
+import numpy
+import math
 
 # Dynamic time warping algorithm
 def DTW(hist_a, hist_b):
@@ -16,25 +20,27 @@ def DTW(hist_a, hist_b):
             cost = abs(hist_a[i] - hist_b[j])
             dtw[i][j] = cost + min([dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1]])
 
-    return dtw[len(hist_a) - 1][len(hist_b) - 1]
+    return dtw[len(hist_a) - 1][len(hist_b) - 1] / (len(hist_a)*len(hist_b))
 
 
 # TODO - Find out \Delta_2(Q) (L2 sensitivity of Q)
 # by running DTW on set of input histograms
-def l2_sensitivity(Q):
-    
+def l2_sensitivity():
+    return 50000
 
 def main():
     # Read in data
     F_k = []
-    with open ("input.txt") as file:
-        line = f.read()
-        F_k.append(int(line))
+    with open ('input.txt') as file:
+        for line in file:
+            F_k.append(int(line))
 
     # Define constants
-    k = len(F_k) # size of histrogram x-axis
-    epsilon = 0.1 #privacy level
-    _lambda = Math.sqrt(k) * l2_sensitivity(Q) / epsilon
+    # size of histrogram x-axis
+    k = len(F_k)
+    # privacy level
+    epsilon = 1
+    _lambda = math.sqrt(k) * l2_sensitivity() / epsilon
     
     # Calculate LPA(F^k, \lambda)
     output = []
@@ -44,19 +50,24 @@ def main():
         output.append(f + noise)
 
     # Generate probablity model
-    len = len(output)
-    for i in range(0, len):
-        output[len] = int(output[len] * 100 / len)
+    length = len(output)
+    for i in range(0, length):
+        output[i] = int(output[i] * 100 / length)
+        print output[i]
+
+    # What if probabilities are negative??
+
+    sys.exit(0)
 
     # Simulation?
     start = trace.pop(0)
     f_addr = start
-    mem_x(start)
+    #mem_x(start)
     for r_addr in trace:
         while(true):
-            rand = int(Math.rand()*len)
+            rand = int(math.rand()*length)
             stride = 0
-            while(rand > output[stride] && stride < len - 1):
+            while(rand > output[stride] and stride < length - 1):
                 rand -= output[stride]
                 stride += 1
             f_addr += stride
@@ -69,3 +80,5 @@ def main():
                 break
             else:
                 mem_x(f_addr)
+
+main()
--- sensitivity.py
+++ sensitivity.py
@@ -4,41 +4,44 @@ import math
 import numpy as np
 import sys
 
-def DTW(interval_a, interval_b):
-    dtw = np.zeros((len(interval_a), len(interval_b)))
+# Dynamic Time Warping (distance) of two trace vectors
+def DTW(vector_a, vector_b):
+    dtw = np.zeros((len(vector_a), len(vector_b)))
 
-    for i in range(0, len(interval_a)):
+    for i in range(0, len(vector_a)):
         dtw[i][0] = sys.maxsize
-    for i in range(0, len(interval_b)):
+    for i in range(0, len(vector_b)):
         dtw[0][i] = sys.maxsize
 
     dtw[0][0] = 0
 
-    for i in range(1, len(interval_a)):
-        for j in range(1, len(interval_b)):
-            cost = abs(interval_a[i] - interval_b[j])
+    for i in range(1, len(vector_a)):
+        for j in range(1, len(vector_b)):
+            cost = abs(vector_a[i] - vector_b[j])
             dtw[i][j] = cost + min([dtw[i-1][j], dtw[i][j-1], dtw[i-1][j-1]])
 
-    return dtw[len(interval_a) - 1][len(interval_b) - 1]
+    ret = dtw[len(vector_a) - 1][len(vector_b) - 1] / (len(vector_a)*len(vector_b))
+
+    return ret
+    
 
 def main():
-
     file_vector = []
-    for file in sys.argv:
+    for i in range (1, len(sys.argv)):
+        file = sys.argv[i]
         with open(file) as f:
             vector = []
             for line in f:
-                vector.append(line)
+                vector.append(int(line))
             file_vector.append(vector)
-            
 
     max = 0
     for i in range(0, len(file_vector)):
-        for(j in range(i, len(file_fector)):
+        for j in range(i + 1, len(file_vector)):
             out = DTW(file_vector[i], file_vector[j])
-            if(out > max)
+            if(out > max):
                 max = out
 
-    print max
+    print "Max:", int(max)
 
 main()
