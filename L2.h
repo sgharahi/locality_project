ef54173be48887605b36566692e0c7e2c834cbdf
rnprakash <rohithnprakash@gmail.com>
2015-05-13 21:17:46 -0500

    Added L2

---
+++ L2.h
@@ -0,0 +1,31 @@
+#ifndef  __L2_H__
+#define  __L2_H__
+
+#include <vector>
+#include <stdlib.h>
+#include <math.h>
+
+#define sqr(x) (x*x)
+
+/* Euclidean distance function. REQUIRES len1 == len2
+ * @input:  list1, the first list
+ *          list2, the second list
+ * @return: integer representing L2 distance between vectors
+ */
+double L2(const std::vector<int64_t> &list_a, const std::vector<int64_t> &list_b)
+{
+    // Local variables
+    int len1 = list_a.size(), len2 = list_b.size();
+    if(len1 != len2){
+        fprintf(stderr, "Vector length mismatch. Aborting.\n");
+        exit(1);
+    }
+
+    double sum = 0;
+    for(int i = 0; i < len1; i++)
+        sum += (sqr(list_a[i]) - sqr(list_b[i]));
+    sum = sqrt(sum);
+    return sum;
+}
+
+#endif //__L2_H__
